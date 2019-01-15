f4cf9e2fd25f3b07f1c1153cf802f7f144e3835f
amjil <amjilt@aliyun.com>
2019-01-15 17:21:40 +0800

    add config file

--- project.clj
+++ project.clj
@@ -3,15 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.4.1"]
+                 [cprop "0.1.13"]
                  [clj-http "3.9.1"]
-                 [clj-time "0.14.4"]
+                 [clj-time "0.15.1"]
                  [enlive "1.1.6"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [mount "0.1.13"]]
+                 [mount "0.1.15"]]
   :plugins [[lein-ancient "0.6.15"]]
   :main ^:skip-aot amjil.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles
+  {:uberjar {:aot :all}
+   :dev           [:project/dev :profiles/dev]
+   :project/dev  {:jvm-opts ["-Dconf=config.edn"]}
+   :profiles/dev {}})
