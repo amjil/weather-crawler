21dc6591d312b1a87e117715347332efe36efe40
amjil <amjilt@aliyun.com>
2020-03-05 22:34:12 +0800

    update denps

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [clj-http "3.8.0"]
-                 [clj-time "0.14.2"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/tools.logging "1.0.0"]
+                 [clj-http "3.10.0"]
+                 [clj-time "0.15.2"]
                  [enlive "1.1.6"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [mount "0.1.12"]]
+                 [mount "0.1.16"]]
   :plugins [[lein-ancient "0.6.15"]]
   :main ^:skip-aot amjil.core
   :target-path "target/%s"
